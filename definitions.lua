afe156f2fbcec1978d7945bd7babeca2f459894e
Andrew Taylor <andrew@utterlyforked.co.uk>
2013-03-03 19:16:36 +0000

    Added some random (but English-like) names

--- definitions.lua
+++ definitions.lua
@@ -1,9 +1,14 @@
 -- define a lua table matching the schema name
 my_table_test = {}
+other_table_test = {}
 
 -- define a function in that table matching the column name to obfuscate
 function my_table_test.email(original_value) 
     return emailAddressAtMailinator(original_value)
+end
+
+function other_table_test.firstname(original_value)
+    return nameLikeFirstname() .. " " .. nameLikeLastname()
 end
 
 function my_table_test.phone(original_value)
@@ -22,3 +27,116 @@ end
 function telephoneNumber(original_value)
     return string.random(string.len(original_value), '%d')
 end
+
+function nameLikeFirstname()
+    local firstnames = {
+"Wilson",
+"Kasha",
+"Lilly",
+"Berneice",
+"Amira",
+"Dorie",
+"Tu",
+"Shira",
+"Elliot",
+"Rona",
+"Velda",
+"Elvera",
+"Arla",
+"Cherryl",
+"Lovella",
+"Ignacia",
+"Damaris",
+"Evelyne",
+"Mariella",
+"Jeanne",
+"Han",
+"Nicole",
+"Catheryn",
+"Lance",
+"Roxanne",
+"Sarita",
+"Hortencia",
+"Sophia",
+"Bree",
+"Rupert",
+"Mabelle",
+"Lynne",
+"Marlene",
+"Amiee",
+"Saundra",
+"Cinderella",
+"Ila",
+"Milford",
+"Grace",
+"Willene",
+"Idalia",
+"Dianna",
+"Taren",
+"Arcelia",
+"Michiko",
+"Shenita",
+"Joeann",
+"Chery",
+"Layla",
+"Randolph",
+    }
+    return firstnames[math.random(1,#firstnames)]
+end
+
+function nameLikeLastname()
+    local lastnames = {
+"Wyer",
+"Kuta",
+"Luster",
+"Bechtol",
+"Adger",
+"Daoust",
+"Tewksbury",
+"Sleeman",
+"Ertel",
+"Ragland",
+"Vassallo",
+"Eppinger",
+"Arwood",
+"Concepcion",
+"Lovell",
+"Isaac",
+"Dallas",
+"Ensley",
+"Meggs",
+"Justis",
+"Hisey",
+"Nobles",
+"Caver",
+"Landen",
+"Rinaldi",
+"Shryock",
+"Hulme",
+"Shuffler",
+"Bethea",
+"Rothenberger",
+"Mourer",
+"Lonzo",
+"Mettler",
+"Addy",
+"Servantes",
+"Cohan",
+"Izzo",
+"Meinhardt",
+"Giltner",
+"Winrow",
+"Inglis",
+"Delk",
+"Turnbow",
+"Alt",
+"Mcdonnell",
+"Schrock",
+"Jarrell",
+"Corriveau",
+"Lorence",
+"Ruple",
+    }
+    return lastnames[math.random(1,#lastnames)]
+end
+
